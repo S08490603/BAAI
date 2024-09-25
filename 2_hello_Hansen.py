f45db8f1c49a0547d87b62da97084f91777ee43b
Hansen <hansenlokanata1@gmail.com>
2024-09-25 17:01:08 +0800

    Add something

---
+++ 2_hello_Hansen.py
@@ -0,0 +1,11 @@
+#
+# Hansen
+# Print Hello Hansen!
+#
+
+# 1. Input
+
+# 2. Process
+
+# 3. Output
+print('Hello Hansen!')
---
+++ 3_hello_name_input.py
@@ -0,0 +1,12 @@
+#
+# Hansen
+# Print Hello Hansen!
+#
+
+# 1. Input
+n = input('Name: ')
+
+# 2. Process
+
+# 3. Output
+print(f'Hello {n}!')

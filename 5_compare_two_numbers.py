14c92976efc558595cb0801a9c6bcd72ccc8877d
S08490603 <hansenlokanata1@gmail.com>
2024-10-09 16:48:08 +0800

    add 2 more task

---
+++ 5_compare_two_numbers.py
@@ -0,0 +1,19 @@
+#
+# Hansen
+# Print Hello Hansen!
+#
+
+# 1. Input
+n1 = int(input('Number 1: '))
+n2 = int(input('Number 2: '))
+
+# 2. Process
+if (n1 < n2):
+    bigger = n2
+elif(n1 > n2):
+    bigger = n1
+else:
+    bigger = 'Same'
+
+# 3. Output
+print(f'Bigger number: {bigger}')

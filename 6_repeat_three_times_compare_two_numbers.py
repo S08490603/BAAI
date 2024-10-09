7b5325d31e91dbabcc0c5ead039b3f82dec0cb3d
S08490603 <hansenlokanata1@gmail.com>
2024-10-09 16:48:21 +0800

    add more

---
+++ 6_repeat_three_times_compare_two_numbers.py
@@ -0,0 +1,25 @@
+#
+# Hansen
+# Repeat three times
+#
+
+max_lap = 3
+curr_lap = 1
+
+while (curr_lap <= max_lap):
+    # 1. Input
+    n1 = int(input('Number 1: '))
+    n2 = int(input('Number 2: '))
+
+    # 2. Process
+    if (n1 < n2):
+        bigger = n2
+    elif(n1 > n2):
+        bigger = n1
+    else:
+        bigger = 'Same'
+
+    # 3. Output
+    print(f'Bigger number: {bigger}')
+
+    curr_lap += 1
